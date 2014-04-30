b64a0e5f83d084e29e6ccdb22c132c6b657ce5b9
unknown <18k6@S136-11.s136>
2014-04-30 15:12:49 +0200

    Initial commit

---
+++ dynamiczne_przegladarka/dynamiczne_przegladarka/dynamiczne_przegladarka.cpp
@@ -0,0 +1,18 @@
+// dynamiczne_przegladarka.cpp : main project file.
+
+#include "stdafx.h"
+#include "Form1.h"
+
+using namespace dynamiczne_przegladarka;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	// Enabling Windows XP visual effects before any controls are created
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false); 
+
+	// Create the main window and run it
+	Application::Run(gcnew Form1());
+	return 0;
+}
